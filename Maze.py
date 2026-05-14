6067aceb9e4404dc95c3a584f72b4be53a61835b
SirBuddyNoah <151926735+SirBuddyNoah@users.noreply.github.com>
2026-05-14 14:58:29 +0100

    adding Cells and Grids
    draws walls from the cell date rather then a recttangle. has a coordinate system and cell class

--- Maze.py
+++ Maze.py
@@ -5,6 +5,7 @@ import sys
 
 
 #Display Settings
+
 Width = 800
 Height = 800
 
@@ -18,12 +19,17 @@ Cell_Width = Width // Columbs
 Cell_Height = Height // Rows
 
 #Pygame Setup
+
 pygame.init()
+
 Screen = pygame.display.set_mode((Width, Height))
+
 pygame.display.set_caption("Maze-Maker 1")
+
 clock = pygame.time.Clock()
 
 #Cells
+
 class Cell:
 
     def __init__(self, row, col):
@@ -34,44 +40,77 @@ class Cell:
         self.visited = False
 
         self.walls = {
+
             "Up": True,
+
             "Down": True,
+
             "Left": True,
+
             "Right": True
+
         }
 
-#Grid construct
-def draw_grid():
-    for row in range(Rows):
-        for col in range(Columbs):
-            x = col * Cell_Height
-            y = row * Cell_Width
+#Grid
+Grid = []
+for row in range(Rows):
 
-            rectangle = pygame.Rect(
-                x,
-                y,
-                Cell_Width,
-                Cell_Height
-            )
-            pygame.draw.rect(
-                Screen,
-                Grid_Colour,
-                rectangle,
-                1
-            )
+    Grid_row = []
+
+    for col in range(Columbs):
+
+        cell = Cell(row, col)
+
+        Grid_row.append(cell)
+
+    Grid.append(Grid_row)
+#Grid construct
+
+def draw_grid():
+
+    for row in Grid:
+
+        for cell in row:
+
+            x = cell.col * Cell_Height
+            y = cell.row * Cell_Width
+
+
+            #Up
+            if cell.walls["Up"]:
+                pygame.draw.line(
+                    Screen,
+                    Grid_Colour,
+                    (x,y),
+                    (x + Cell_Width, y),
+                    2
+                )
+               
+            #Down
+            #Left
+            #Right
+
 
 #Game Loop
+
 Running = True
+
 while Running:
+
     for event in pygame.event.get():
+
         if event.type == pygame.QUIT:
+
             Running = False
 
     Screen.fill(Background_Colour)
+
     draw_grid()
+
     pygame.display.update()
 
     clock.tick(60)
 
 pygame.quit()
+
 sys.exit()
